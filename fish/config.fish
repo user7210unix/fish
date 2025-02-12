60b48599ef63bbcee4e0c39c287214f8a97b1998
user7210unix <user7210@proton.me>
2025-02-12 15:05:52 +0100

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,3 @@
-# Begin ~/.config/fish/config.fish
-
-# Set up prompt
 set -g fish_color_normal white
 set -g fish_color_command green
 set -g fish_color_redirection yellow
@@ -10,35 +7,37 @@ set -g fish_color_command_substitution magenta
 set -g fish_color_operator yellow
 set -g fish_color_argument blue
 
-# Set custom prompt format (similar to PS1 in bash)
 function fish_prompt
-    set_color yellow
-    echo -n ' '  # Folder icon
-    set_color blue
-    echo -n (prompt_pwd)  # Current directory
-    echo -n ' '
-    set_color green
-    echo -n '# '
     set_color normal
+    echo -n (basename (pwd))
+    set_color normal
+    echo -n '  '
+
+    set_color blue
+    echo -n ' '
+    set_color normal
+    echo -n '  '
 end
 
-
-# Aliases (similar to bash aliases)
-alias ls='ls --color=auto'
+alias ls='ls -a --color=auto'
 alias grep='grep --color=auto'
-alias fetch='/home/user/Documents/tf-main/./tf.py'
+alias fetch='pfetch'
 alias c='clear'
-alias os='/home/user/banan-os/./bos qemu'
-alias install='sudo nala install -y'
-alias top='btm --theme nord'
-alias off='sudo poweroff now'
-alias update='sudo nala update && sudo nala upgrade'
-alias l='ls -la'
+alias off='sudo poweroff'
+
+# alias install='sudo nala install -y'
+# alias install='sudo emerge'
+# alias install='sudo pacman -Sy --noconfirm'
+# alias install='doas pkg install'
+
+# alias update='sudo nala update && sudo nala upgrade'
+# alias update='sudo emerge sync'
+# alias update='sudo pacman -Syu --noconfirm'
+# alias update='doas pkg update'
+
 alias clone 'git clone --depth 1'
 alias merge 'xrdb ~/.Xresources'
 
-# Fish plugins installation via Oh My Fish
-# Install useful plugins
-set -g fish_greeting '' 
 
-# End ~/.config/fish/config.fish
+set -g fish_greeting '' 
+clear
